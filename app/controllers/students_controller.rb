20b6f90621bf182c71964f8bb41f2934c7351339
Srikalyan Megati <kalmegati@gmail.com>
2020-02-03 15:45:27 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,15 +14,25 @@ class StudentsController < ApplicationController
     end
   end
   
+<<<<<<< HEAD
   def activate
+=======
+  def activate_student
+    binding.pry
+>>>>>>> da222fba184d4b6c9e5f4cc53ca318ccb5e57991
     @student = Student.find(params[:id])
     if @student.active
       @student.active = false
     else
       @student.active = true
     end
+<<<<<<< HEAD
     @student.save
     redirect_to student_path(@student)
+=======
+    binding.pry
+    @student.save
+>>>>>>> da222fba184d4b6c9e5f4cc53ca318ccb5e57991
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+<<<<<<< HEAD
   resources :students, only: [:index, :show, :activate_student]
   
   get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
+=======
+  resources :students, only: [:index, :show]
+  
+  get '/students/:id/activate', to: 'students#activate_students'
+>>>>>>> da222fba184d4b6c9e5f4cc53ca318ccb5e57991
   
 end
